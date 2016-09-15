3c03d71d81bdf21a482f16fe23d3a4025ed47a29
lfaler <laynefaler@gmail.com>
2016-09-15 12:46:33 -0700

    Refactoring src with remade functions

---
+++ new-src/arrays/arrays.js
@@ -0,0 +1,24 @@
+/**
+* @Author: Layne Faler <laynefaler>
+* @Date:   09-09-2016
+* @Email:  laynefaler@gmail.com
+* @Last modified by:   laynefaler
+* @Last modified time: 09-15-2016
+*/
+
+/*
+
+****** ARRAYS ********
+
+pre-made functions
+To chain with others
+
+**********************
+
+*/
+
+// clear
+
+Array.prototype.cnClear = function() {
+  return [];
+}
---
+++ new-src/functions/functions.js
@@ -0,0 +1,7 @@
+/**
+* @Author: Layne Faler <laynefaler>
+* @Date:   09-15-2016
+* @Email:  laynefaler@gmail.com
+* @Last modified by:   laynefaler
+* @Last modified time: 09-15-2016
+*/
